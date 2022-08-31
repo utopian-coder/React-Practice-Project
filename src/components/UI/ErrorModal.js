73f52fb4084372d08b098790b9280e040db04d42
Imran Biswas <imranbiswas2209@gmail.com>
2022-08-31 09:27:40 +0530

    Fix error modal issues!

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,11 +1,16 @@
 import styles from "./ErrorModal.module.css";
 import Card from "./Card";
 import Button from "./Button";
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
 
 const ErrorModal = (props) => {
-  return (
-    <div>
-      <div className={styles.backdrop} onClick={props.onConfirm} />
+  const BackDrop = (props) => {
+    return <div className={styles.backdrop} onClick={props.onConfirm} />;
+  };
+
+  const Modal = (props) => {
+    return (
       <Card className={styles.modal}>
         <header className={styles.header}>
           <h2>{props.title}</h2>
@@ -17,7 +22,25 @@ const ErrorModal = (props) => {
           <Button onClick={props.onConfirm}>Ok</Button>
         </footer>
       </Card>
-    </div>
+    );
+  };
+
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <BackDrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+
+      {ReactDOM.createPortal(
+        <Modal
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </Fragment>
   );
 };
 
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import styles from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
@@ -39,7 +40,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           onConfirm={errorHandler}
@@ -68,7 +69,7 @@ const AddUser = (props) => {
           <Button type='submit'>Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
